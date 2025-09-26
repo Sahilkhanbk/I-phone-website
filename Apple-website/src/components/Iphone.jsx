69f941e49f64d724e413eb4d11666b1177fd3925
sahil khan <139511550+Sahilkhanbk@users.noreply.github.com>
2025-09-27 04:36:08 +0530

    Update Iphone.jsx

--- Apple-website/src/components/Iphone.jsx
+++ Apple-website/src/components/Iphone.jsx
@@ -11,7 +11,6 @@ Title: Apple iPhone 15 Pro Max Black
 
 
 
-
 import * as THREE from 'three';
 import React, { useEffect, useRef } from "react";
 import { useGLTF, useTexture } from "@react-three/drei";
@@ -266,3 +265,4 @@ export default Model;
 
 
 useGLTF.preload("/models/scene.glb");
+
